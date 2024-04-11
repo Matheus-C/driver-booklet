8bb1ff747114b43795ac131903843fbf4a4e59e7
Guilherme Mendes <guilhermemendes789@gmail.com>
2024-04-11 22:45:59 +0100

    + connection to mysql db

--- app/models/database.py
+++ app/models/database.py
@@ -3,9 +3,17 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, distinct ,func ,or_,extract
 from sqlalchemy.orm import sessionmaker,aliased
 
-engine = create_engine(f"sqlite:///{os.getenv('path_to_db_web_app')}",
-                        echo=False,
-                        connect_args={'check_same_thread': False},
-                        pool_size=20, max_overflow=0)
+load_dotenv()
+
+
+user_db = os.environ.get('user_db')
+pass_db = os.environ.get('pass_db')
+host_db = os.environ.get('host_db')
+
+# engine = create_engine(f"sqlite:///{os.getenv('path_to_db_web_app')}",
+#                         echo=False,
+#                         connect_args={'check_same_thread': False},
+#                         pool_size=20, max_overflow=0)
+engine = create_engine(f"mysql+mysqlconnector://{user_db}:{pass_db}@{host_db}")
 Session = sessionmaker(bind=engine)
 session = Session()
