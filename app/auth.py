d3c62fcea8a28c00838b28e20a947b5b07fbc2f4
Guilherme Mendes <guilhermemendes789@gmail.com>
2024-04-12 21:01:10 +0100

    cleaning up auth + database // signup working (initial rls)

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import render_template,url_for,request,jsonify,send_file,redirect,flash
 from app.models.models import *
 from app.models.database import *
-from app import app,login_manager
+from app import app,login_manager,bcrypt
 from flask_login import login_user,logout_user,current_user,login_required
 
 @login_manager.user_loader
@@ -17,11 +17,34 @@ def index():
     else:
         return 'No user logged'
 
-@app.route('/login',methods=['POST'])
-def login(id):
-    user = session.query(User).filter_by(id=id).first()
-    login_user(user)
-    return 'Success'
+@app.route('/login',methods=['GET','POST'])
+def login(id=None):
+    if request.method == 'GET':
+        return render_template('htmx/login.html')
+    elif request.method == 'POST' and request.form:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        hashed_password = bcrypt.generate_password_hash(password=password)
+        user = session.query(User).filter_by(email=email,password=hashed_password).first()
+        login_user(user)
+        return redirect('/')
+    
+
+@app.route('/signup',methods=['GET','POST'])
+def signup(id=None):
+    if request.method == 'GET':
+        return render_template('htmx/signup.html')
+    
+    elif request.method == 'POST' and request.form:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        hashed_password = bcrypt.generate_password_hash(password=password)
+        user = User(email=email,password=hashed_password)
+        session = Session()
+        user = session.add(user)
+        session.commit()
+        return redirect('/')
 
 @app.route('/logout')
 def logout():
--- app/models/database.py
+++ app/models/database.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, distinct ,func ,or_,extract
 from sqlalchemy.orm import sessionmaker,aliased
-
 load_dotenv()
 
 
@@ -10,10 +9,6 @@ user_db = os.environ.get('user_db')
 pass_db = os.environ.get('pass_db')
 host_db = os.environ.get('host_db')
 
-# engine = create_engine(f"sqlite:///{os.getenv('path_to_db_web_app')}",
-#                         echo=False,
-#                         connect_args={'check_same_thread': False},
-#                         pool_size=20, max_overflow=0)
 engine = create_engine(f"mysql+mysqlconnector://{user_db}:{pass_db}@{host_db}")
 Session = sessionmaker(bind=engine)
 session = Session()
