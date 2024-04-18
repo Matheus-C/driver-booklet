24c1d2d83031f0c26297ca97dae432d385f6b4aa
Matheus-C <matheuscustodio@id.uff.br>
2024-04-18 18:20:00 -0300

    new model VehicleEvent and minor fixes

--- app/models/models.py
+++ app/models/models.py
@@ -64,9 +64,11 @@ class Event(Base):
   idUser = Column(Integer, ForeignKey('users.id'))
   IdVehicle = Column(Integer, ForeignKey('vehicle.id'))
   IdCompany = Column(Integer, ForeignKey('company.id'))
+  geolocation = Column('geolocation', String(512))
+  idNote = Column(Integer, ForeignKey('notes.id'))
 
-class Notes(Base):
-  __tablename__ = 'notes'
+class Attachment(Base):
+  __tablename__ = 'attachment'
   id = Column('id', Integer, primary_key=True)
   idUser = Column(Integer, ForeignKey('users.id'))
   idType = Column(Integer, ForeignKey('eventType.id'))
@@ -105,5 +107,18 @@ class Vehicle(Base):
   idCompany = Column(Integer, ForeignKey('company.id'))
   model = Column('model', String(255))
   licensePlate = Column('licensePlate', String(255))
+
+class VehicleEvent(Base):
+  __tablename__ = 'vehicleEvent'
+  id = Column('id', Integer, primary_key=True)
+  eventTimestamp = Column('eventTime', DateTime)
+  idType = Column(Integer, ForeignKey('eventType.id'))
+  idUser = Column(Integer, ForeignKey('users.id'))
+  IdVehicle = Column(Integer, ForeignKey('vehicle.id'))
+  IdCompany = Column(Integer, ForeignKey('company.id'))
+  mileage = Column('mileage', Float)
+  idAttachment = Column('idAttachment', ForeignKey('attachment.id'))
+
+
 
 Base.metadata.create_all(engine)
