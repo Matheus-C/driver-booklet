4ead3f3f889fbf683409407266dda009cd3a4a94
Guilherme Mendes <guilhermemendes789@gmail.com>
2024-04-20 20:13:35 +0100

    fixes on initial version of the report

--- app/reports.py
+++ app/reports.py
@@ -15,41 +15,46 @@ def reports():
         
         if request.method == 'POST':
             # get companies, that you worked for and cars ?
+            if request.form:
+                ## needs validation before querying
+                dict_data = request.form.to_dict()
 
-            ## needs validation before querying
-            query = f"""
-            WITH event_query as 
-            (SELECT e.eventTime date_start,
-                    et.category,
-                    case when et.name like '%_end' then 0
-                    ELSE LEAD(eventTime, 1, 0) OVER (PARTITION BY et.name ORDER BY eventTime ASC) 
-                    END as date_end,
-                    e.idVehicle,
-                    e.idCompany,
-                    e.geolocation,
-                    e.idAttachment
-            FROM `event` e
-            INNER JOIN `eventType` et ON et.id = e.idType
-            WHERE idUser = {current_user.id})
-        
-        select 
-            c.name CompanyName
-            ,e.date_start
-            ,e.category CategoryName
-            ,date_end      
-            ,TIMESTAMPDIFF(SECOND,date_start,date_end)/3600 AS timeSpent
-            ,v.model
-            ,v.licensePlate 
-        from event_query e
-        inner join vehicle v on v.idCompany = e.idCompany and e.idVehicle = v.id
-        inner join company c on c.id = e.idCompany
-        where date_end <> 0
-            order by date_start desc
-            """
+                query = f"""
+                WITH event_query as 
+                (SELECT e.eventTime dateStart,
+                        et.category,
+                        case when et.name like '%_end' then 0
+                        ELSE LEAD(eventTime, 1, 0) OVER (PARTITION BY et.category ORDER BY eventTime ASC) 
+                        END as dateEnd,
+                        e.idVehicle,
+                        e.idCompany,
+                        e.geolocation,
+                        e.idAttachment
+                FROM `event` e
+                INNER JOIN `eventType` et ON et.id = e.idType
+                WHERE idUser = {current_user.id}
+                and e.idCompany = {dict_data['idCompany']}
+                and e.eventTime between (date('{dict_data['dateStart']}')) and (date('{dict_data['dateEnd']}') + 1)
+                )
             
-        query = text(query)
-        session = Session()
-        data = session.execute(query).fetchmany(5)
-        session.close()
+            select 
+                c.name companyName
+                ,e.dateStart
+                ,e.category categoryName
+                ,dateEnd      
+                ,TIMESTAMPDIFF(SECOND,dateStart,dateEnd)/3600 AS timeSpent
+                ,v.model
+                ,v.licensePlate 
+            from event_query e
+                left join vehicle v on v.idCompany = e.idCompany and e.idVehicle = v.id
+                left join company c on c.id = e.idCompany
+            where dateEnd <> 0
+            order by dateStart asc
+                """
+                
+            query = text(query)
+            session = Session()
+            data = session.execute(query).fetchmany(5)
+            session.close()
 
-        return render_template('htmx/report.html',data=data)
+            return render_template('htmx/report.html',data=data)
