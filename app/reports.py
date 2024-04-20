2dc46f06b80fda9eb66390c33f5b8887e1e40a2c
Guilherme Mendes <guilhermemendes789@gmail.com>
2024-04-20 19:24:03 +0100

    initial version of the report

--- app/reports.py
+++ app/reports.py
@@ -3,6 +3,7 @@ from app.models.models import *
 from app.models.database import *
 from app import app
 from flask_login import current_user,login_required
+from sqlalchemy.sql import text
 
 
 @app.route("/reports", methods=["GET","POST"])
@@ -14,9 +15,41 @@ def reports():
         
         if request.method == 'POST':
             # get companies, that you worked for and cars ?
-            session = Session()
-            
-            return 'pass'
 
+            ## needs validation before querying
+            query = f"""
+            WITH event_query as 
+            (SELECT e.eventTime date_start,
+                    et.category,
+                    case when et.name like '%_end' then 0
+                    ELSE LEAD(eventTime, 1, 0) OVER (PARTITION BY et.name ORDER BY eventTime ASC) 
+                    END as date_end,
+                    e.idVehicle,
+                    e.idCompany,
+                    e.geolocation,
+                    e.idAttachment
+            FROM `event` e
+            INNER JOIN `eventType` et ON et.id = e.idType
+            WHERE idUser = {current_user.id})
+        
+        select 
+            c.name CompanyName
+            ,e.date_start
+            ,e.category CategoryName
+            ,date_end      
+            ,TIMESTAMPDIFF(SECOND,date_start,date_end)/3600 AS timeSpent
+            ,v.model
+            ,v.licensePlate 
+        from event_query e
+        inner join vehicle v on v.idCompany = e.idCompany and e.idVehicle = v.id
+        inner join company c on c.id = e.idCompany
+        where date_end <> 0
+            order by date_start desc
+            """
             
+        query = text(query)
+        session = Session()
+        data = session.execute(query).fetchmany(5)
+        session.close()
 
+        return render_template('htmx/report.html',data=data)
